b65164017de788a4d1df56d9a5291c491764da6f
hossainmamun <hossainmamun337@gmailcom>
2022-10-12 20:50:12 +0600

    navbar update

--- src/Components/DefaultPage/NavBar.jsx
+++ src/Components/DefaultPage/NavBar.jsx
@@ -18,11 +18,11 @@ const NavBar = () => {
     return (
         <div className=' w-full fixed top-0 left-0 z-10'>
             <div className='font-bold font-mono lg:flex items-center justify-between bg-white py-2 px-3 sm:px-3.5 md:px-7 lg:px-14 xl:px-24'>
-                <div>
+                <Link to='/'>
                     <span>
                         <img src={brand} alt='brand img' className='w-44' />
                     </span>
-                </div>
+                </Link>
 
                 <div
                     onClick={() => setOpen(!open)}
@@ -31,15 +31,20 @@ const NavBar = () => {
                 </div>
 
                 <ul
-                    className={`lg:flex lg:items-center lg:pb-0 pb-12 absolute lg:static bg-white lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'
-                        }`}>
+                    className={`lg:flex lg:items-center lg:pb-0 pb-12 absolute lg:static bg-white lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in ${
+                        open ? 'top-16 ' : 'top-[-490px]'
+                    }`}>
                     {Links.map((link) => (
                         <li
                             key={link.name}
                             className='lg:ml-8 text-base lg:my-0 my-7'>
                             <Link
                                 to={link.link}
-                                className='text-[#106731] duration-500 hover:text-black'>
+                                className={`${
+                                    link.name === 'LOGIN/REGISTER'
+                                        ? 'border-2 text-[#106731] border-[#106731] px-4 py-2 rounded-lg uppercase hover:text-white hover:bg-[#106731] duration-200'
+                                        : 'text-[#106731] duration-500 hover:text-black'
+                                }`}>
                                 {link.name}
                             </Link>
                         </li>
