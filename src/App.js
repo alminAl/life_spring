62a5251957a473561e61c4c88609c671fb41746d
Md. Riajul Islam <riaj17021995@gmail.com>
2022-10-22 14:53:30 +0600

    route protect added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import BookAppointment from './Components/BookAppointment/BookAppointment.jsx';
 import CourseDetails from './Components/Dashboard/CourseDetails.jsx';
 import Courses from './Components/Dashboard/Courses.jsx';
 import Dashboard from './Components/Dashboard/Dashboard.jsx';
 import Profile from './Components/Dashboard/Profile.jsx';
 import Error from './Components/Error/Error.jsx';
+import { useAuthContext } from './hooks/useAuthContext.jsx';
 import Layout from './Layout/Layout.jsx';
 import Home from './Pages/Home.jsx';
 import Login from './Pages/Login.jsx';
@@ -12,34 +13,58 @@ import Professionals from './Pages/Professionals.jsx';
 import Signup from './Pages/Signup.jsx';
 
 function App() {
+    const { user } = useAuthContext();
     return (
         <div>
             <BrowserRouter>
                 <Layout>
                     <Routes>
                         <Route path='/' element={<Home />} />
-                        <Route index element={<Home />} />
-                        <Route path='/login' element={<Login></Login>}></Route>
+                        <Route
+                            path='/login'
+                            element={
+                                !user ? <Login /> : <Navigate to='/dashboard/profile' />
+                            }></Route>
                         <Route
                             path='/signup'
-                            element={<Signup></Signup>}></Route>
+                            element={
+                                !user ? <Signup /> : <Navigate to='/dashboard/profile' />
+                            }></Route>
                         <Route
                             path='/professionals'
-                            element={<Professionals />}></Route>
-                        <Route path='*' element={<Error />} />
+                            element={
+                                user ? (
+                                    <Professionals />
+                                ) : (
+                                    <Navigate to='/login' />
+                                )
+                            }></Route>
+
                         <Route
                             path='/professionals/bookAppointment/:professionalsId'
-                            element={<BookAppointment />}
+                            element={
+                                user ? (
+                                    <BookAppointment />
+                                ) : (
+                                    <Navigate to='/login' />
+                                )
+                            }
                         />
 
                         {/* dashboard */}
                         <Route
-                            path='life_spring_dashboard'
-                            element={<Dashboard />}>
-                            <Route path='profile' element={<Profile />} />
-                            <Route path='courses' element={<Courses />} />
-                            <Route path='courses/:id' element={<CourseDetails />} />
+                            path='dashboard'
+                            element={
+                                user ? <Dashboard /> : <Navigate to='/login' />
+                            }>
+                            <Route path='profile' element={user ? <Profile /> : <Navigate to='/login' />} />
+                            <Route path='courses' element={user ? <Courses /> : <Navigate to='/login' />} />
+                            <Route
+                                path='courses/:id'
+                                element={user ? <CourseDetails /> : <Navigate to='/login' />}
+                            />
                         </Route>
+                        <Route path='*' element={<Error />} />
                     </Routes>
                 </Layout>
             </BrowserRouter>
